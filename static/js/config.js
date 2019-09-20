d8a793e44e41cc83a5c5584aedae969c934df8ab
Jason Thomas <jthomas.jason@gmail.com>
2019-09-19 19:49:59 -0500

    first

---
+++ static/js/config.js
@@ -0,0 +1,15 @@
+// API key
+var API_KEY = "pk.eyJ1IjoidGhvbWExNzUiLCJhIjoiY2swOXA4ZjN5MGI5ajNjcGNwdXFva29paSJ9.NBNKp3wirb2cMqTuaVhtgQ";
+const attribution = `
+  Map data & copy;
+  <a href="https://www.openstreetmap.org/">
+    OpenStreetMap
+    </a>
+  contributors,
+    <a href="https://creativecommons.org/licenses/by-sa/2.0/">
+      CC-BY-SA
+    </a>
+    , Imagery ©
+  <a href="https://www.mapbox.com/">
+    Mapbox
+    </a>`;
